5e475b7fe4da3297327c8e430b374ba56a2bc88d
Yu <549303468@qq.com>
2023-08-14 03:57:44 -0700

    finish

--- beginning_python/chapter13/13.2.1.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("somedatabase.db")
-curs = conn.cursor()
-conn.commit()
-conn.close()
-
--- beginning_python/chapter16/area.py
+++
@@ -1,2 +0,0 @@
-def rect_area(height, width):
-    return height * height
